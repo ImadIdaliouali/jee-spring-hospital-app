74f2e8fd3580440a0c3816d8ec70fcf0df992c6f
ImadIdaliouali <imadidaliouali@gmail.com>
2025-03-24 12:00:33 +0000

    Implement delete operation

--- src/main/java/ma/enset/jeespringhospitalapp/web/PatientController.java
+++ src/main/java/ma/enset/jeespringhospitalapp/web/PatientController.java
@@ -27,4 +27,10 @@ public class PatientController {
         model.addAttribute("keyword", keyword);
          return "patients";
     }
+
+    @GetMapping("/delete")
+    public String deletePatient(Long id, String keyword, int page) {
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
 }
