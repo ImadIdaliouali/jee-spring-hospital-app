eda0d349666480f6bf4bc063b390265a6ee64b0b
ImadIdaliouali <imadidaliouali@gmail.com>
2025-03-24 11:18:17 +0000

    Implement pagination functionality

--- src/main/java/ma/enset/jeespringhospitalapp/web/PatientController.java
+++ src/main/java/ma/enset/jeespringhospitalapp/web/PatientController.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import ma.enset.jeespringhospitalapp.entites.Patient;
 import ma.enset.jeespringhospitalapp.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,9 +19,11 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patientList = patientRepository.findAll();
-        model.addAttribute("listPatients", patientList);
-        return "patients";
+    public String index(Model model, @RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "4") int size) {
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+         return "patients";
     }
 }
