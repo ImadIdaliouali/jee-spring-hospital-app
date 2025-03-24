a54f829b578b8fd4c6dfaaa31f604313abc161f5
ImadIdaliouali <imadidaliouali@gmail.com>
2025-03-24 11:41:25 +0000

    Implement search functionality

--- src/main/java/ma/enset/jeespringhospitalapp/repository/PatientRepository.java
+++ src/main/java/ma/enset/jeespringhospitalapp/repository/PatientRepository.java
@@ -1,7 +1,10 @@
 package ma.enset.jeespringhospitalapp.repository;
 
 import ma.enset.jeespringhospitalapp.entites.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContains(String keyword, Pageable pageable);
 }
